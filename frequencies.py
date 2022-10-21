77702370824716fbe953340b7764a7ff5ecd27b8
Thang Nguyen <thang.1.nguyen@kcl.ac.uk>
2022-10-21 12:34:52 +0100

    Add Solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
+"Frequencies function."
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies.update({item: frequencies.get(item) + 1})
+        else:
+            frequencies.update({item: 1})
     return frequencies
